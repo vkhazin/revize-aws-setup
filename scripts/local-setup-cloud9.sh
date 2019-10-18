a1ae92db38cf62c22bd4e550e0ee5b78f52f8a47
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2019-10-18 17:22:24 -0400

    updating cloud9 script

--- scripts/local-setup-cloud9.sh
+++ scripts/local-setup-cloud9.sh
@@ -6,9 +6,9 @@ source ./envs/global.sh
 echo 'Installing system dependencies...'
 
 # Install Terraform
-curl "https://releases.hashicorp.com/terraform/$terraform_version/terraform_${terraform_version}_linux_amd64.zip" -o terraform_${terraform_version}_darwin_amd64.zip
-sudo unzip -o terraform_${terraform_version}_darwin_amd64.zip -d $terraform_install_dir
-rm -f ./terraform_*.zip
+curl "https://releases.hashicorp.com/terraform/$terraform_version/terraform_${terraform_version}_linux_amd64.zip" -o terraform.zip
+sudo unzip -o terraform.zip -d $terraform_install_dir
+rm -f ./terraform*.zip
 ${terraform_install_dir}/terraform version
 
 aws configure
